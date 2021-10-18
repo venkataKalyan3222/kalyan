cb6e573b0816d15e4848411868d94b0bbb3c2fac
venkatakalyan3222 <venkatakalyan3222@gmail.com>
2021-10-18 11:24:18 +0530

    one.js

---
+++ Html/one.js
@@ -0,0 +1,12 @@
+let product_name ="Solid Men Mandarin Collar Blue T-Shirt"
+let brand_name ="FastColors"
+let price = 611;
+let rating = 3.9;
+let stock = true;
+let size =["36","38","40"]
+console.log(typeof product_name);
+console.log(typeof brand_name);
+console.log(typeof price);
+console.log(typeof rating);
+console.log(typeof stock);
+console.log(typeof size);
